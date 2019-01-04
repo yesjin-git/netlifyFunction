ae3bdcd140790f09c66964bcf95c973fc2b1cf79
Eungjin <tiuln2010@gmail.com>
2019-01-05 00:34:04 +0900

    .

--- src/hello.js
+++ src/hello.js
@@ -1,7 +1,25 @@
-exports.handler = function(event, context, callback) {
+exports.handler = function(event,context,callback) {
   console.log(context)
-  callback(null, {
+  callback(null,{
     statusCode: 200,
-    body: "Hello, World" + context
-    });  
+    body: `Hello, World
+     ${allKeys(context)}`
+  });
+}
+
+function allKeys(obj,array) {
+  if(typeof array === 'undefined' || typeof array === null) {
+    array = new Array();
   }
+  for(var property in obj) {
+    if(obj.hasOwnProperty(property)) {
+      if(typeof obj[property] === "object") {
+        array.push(property);
+        array.concat(allKeys(obj[property],array));
+      } else {
+        array.push(property);
+      }
+    }
+  }
+  return array;
+}
